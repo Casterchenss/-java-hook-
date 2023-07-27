e84f6783aad0db04acb18bc270fff0a0e40fe2da
Casterchenss <18470131120@163.com>
2023-07-27 08:06:25 +0800

    First

---
+++ hookClassAllMethods.js
@@ -0,0 +1,62 @@
+function logInf(classs){
+    // console.log(String(classs.getName()))
+    console.log(classs)
+    // hookClass(String(classs.getName()))
+}
+function getAllsonClass(classs){
+    console.log('\n')
+    console.log("查询到子类  =>" + classs.getName())
+    hookClass(String(classs.getName()))
+}
+var thisclass = null;
+//"java.security.MessageDigest"
+function hookClass(CLASS){
+Java.perform(function(){
+    var classStudent = Java.use(CLASS);
+    var classs = classStudent.class;
+
+    //获取所有内部类
+    var innerClasses = classs.getDeclaredClasses();
+    if(innerClasses.length > 0){
+        innerClasses.forEach(getAllsonClass);
+    }
+    console.log("===========" + classs + "中的所有变量==============")
+    //输出所有变量
+    classs.getDeclaredFields().forEach(logInf)
+    console.log("===========" + classs +  "的所有方法==============")
+    //输出所有方法,并hook
+    classs.getDeclaredMethods().forEach(function(method){
+        console.log(method)
+       var methodsName = method.getName();
+       var overloads  = classStudent[methodsName].overloads;
+    //    console.log(overloads.length)
+       for (var i=0; i< overloads.length; i++){
+            overloads[i].implementation = function () {
+            console.log('\n')
+            console.warn("进入" + classs.getName() + "类的" + methodsName + "方法")
+            for(var j=0; j<arguments.length; j++){
+                console.error("参数" + j + " => " + arguments[j])
+            }
+            if (arguments.length === 0) {
+              console.log("该函数无参数");
+            }
+            var result = this[methodsName].apply(this,arguments)
+            console.error("结果是 => " + result)
+            return result;
+            };
+        }
+    })
+    console.log('\n')
+})
+}
+function main(){
+//只需要在此处填写包名.类名即可
+    try {
+        hookClass("com.example.myapplication4.MainActivity")
+    }catch (e) {
+        console.log("没有找到该类")
+    }
+//hookClass("javax.crypto.Cipher")
+}
+
+setImmediate(main)
